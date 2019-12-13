34a678552b09d1b532c236f2f7b495101ca509d9
Benjamin Rochez <rochez.benjamin@gmail.com>
2019-12-13 08:58:36 +0100

    Added the followed stream informations

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="hello">
-    <h2>Twitchers: {{lives}}</h2>
+    
+    <div v-if="!loading">
+      <h2>Twitchers you follow: </h2>
+      <ul>
+        <li v-for="acti in activ" v-bind:key="acti.user_id">{{acti.username}} {{acti.status}}</li>
+      </ul>
+      <h2>Twitchers you follow that are live</h2>
+      <ul>
+        <li v-for="live in lives" v-bind:key="live.id">{{live}}</li>
+      </ul>
+    </div>
     <input type="text" name="username" id="username" v-model="username" >
     <button @click="getId">getId</button>
-    <button @click="getVideo">getId</button>
+    <button @click="getVideo">getVideos</button>
+    <button @click="getFollows">getFollows</button>
   </div>
 </template>
 
 <script>
-import db from '@/firebase/init'
+//import db from '@/firebase/init'
 import axios from 'axios'
 export default {
   name: 'HelloWorld',
@@ -18,6 +29,9 @@ export default {
       id:'',
       lives: [],
       videos: [],
+      followed: [],
+      deactivated: [],
+      loading: true,
       config: {
           headers: {'Client-ID': 'bj4n17r5g7ztuog08idhnllq04yivm'}
       }
@@ -25,6 +39,12 @@ export default {
   },
   props: {
     msg: String
+  },
+  computed: {
+    activ: function(){
+      return this.followed
+      .filter(item => !this.deactivated.includes(item.username))
+    }
   },
   methods:{
     getId(){
@@ -34,7 +54,6 @@ export default {
         .then((res) =>{
           this.id = res.data.data[0].id;
           console.log('Id: ' + this.id);
-          
         }).catch((err) =>{
           console.log(err);
         })
@@ -47,34 +66,66 @@ export default {
         }).catch((err) =>{
           console.log(err);
         })
-    }
+    },
 
-  },
-  created(){
-    db.collection('twitcher').get() 
-    .then(snapshot => {
-      snapshot.forEach(doc =>{
-        let single_twitcher = doc.data()
-        single_twitcher.id = doc.id
-        
-        axios.get(`https://api.twitch.tv/helix/streams?user_id=${single_twitcher.user_id}`, this.config)
+    getFollows(){
+      // Retreive following list
+      axios.get(`https://api.twitch.tv/helix/users/follows?from_id=${this.id}&first=99`, this.config)
         .then((res) =>{
-          console.log(res);
-          if(res.data.data[0]){
-            single_twitcher.status = res.data.data[0].type;
-          } else{
-            single_twitcher.status = "Not Live";
-          }
-          this.lives.push(single_twitcher)
+          
+          let singles_twitcher = res.data.data;
+          
+          singles_twitcher.forEach(doc=>{  
+            
+            let to_name = doc.to_name;
+            let the_user_id = doc.to_id;
+
+            // Get the following list account info to check if they are deactivated
+            axios.get(`https://api.twitch.tv/helix/users?login=${doc.to_name}`, this.config)
+            .then((res) =>{
+              
+              // If deactivated or banned
+              if(res.data.data.length > 0){
+                let user_id = res.data.data[0].id;
+                // Retreive their live infos
+                 axios.get(`https://api.twitch.tv/helix/streams?user_id=${user_id}`, this.config)
+                .then((res)=>{
+                  
+                  // est en live
+                  if(res.data.data[0]){
+                    this.followed.push({
+                      'username': to_name, 
+                      'status': 'live', 
+                      'user_id': the_user_id})
+                    this.lives.push(res.data.data[0]);
+                    this.loading = false;
+                  }else{
+                    this.followed.push({
+                      'username': to_name, 
+                      'status': 'not live', 
+                      'user_id': the_user_id})
+                    this.loading = false;
+                  }
+                })
+                .catch((err) =>{
+                  console.log(err);
+                })
+
+              }else{
+                this.deactivated.push(doc.to_name)
+              }
+            })
+            .catch((err) =>{
+              console.log(err);
+            })            
+          })
+          
         }).catch((err) =>{
           console.log(err);
         })
+    }
 
-
-        
-      })
-    })
-  }
+  },
 }
 </script>
 
